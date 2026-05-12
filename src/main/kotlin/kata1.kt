f2ec90b09b0f8747fc0235dd4c15f173f00f9a52
Enrique López Mañas <eenriquelopez@gmail.com>
2026-05-12 12:13:35 +0200

    Solution firt Kata

--- src/main/kotlin/kata1.kt
+++ src/main/kotlin/kata1.kt
@@ -2,15 +2,15 @@ package org.example
 
 import java.lang.IllegalArgumentException
 
-fun failWithWrongAge(age: Int?) /* */   {
+fun failWithWrongAge(age: Int?)  : Nothing  {
     throw IllegalArgumentException("Wrong age: $age")
 }
 
 fun checkAge(age: Int?) {
     if (age == null || age !in 0..150) failWithWrongAge(age)
-    println("Congrats! Next year you'll be ${age?.plus(1)}.")
+    println("Congrats! Next year you'll be ${age + 1}.")
 }
 
 fun main() {
-    checkAge(10)
+    checkAge(200)
 }
