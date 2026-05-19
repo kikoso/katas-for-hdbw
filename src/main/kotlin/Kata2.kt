4aa0ec0f2b07cddd21ad1060fa813b25d1b8bbbb
Enrique López Mañas <eenriquelopez@gmail.com>
2026-05-19 09:38:23 +0200

    Solution from second Kata

--- src/main/kotlin/Kata2.kt
+++ src/main/kotlin/Kata2.kt
@@ -1,6 +1,6 @@
 package org.example
 
-class Person
+data class Person(val name: String, val age: Int)
 
 fun getPeople(): List<Person> {
     return listOf(Person("Alice", 29), Person("Bob", 31))
@@ -10,4 +10,8 @@ fun comparePeople(): Boolean {
     val p1 = Person("Alice", 29)
     val p2 = Person("Alice", 29)
     return p1 == p2  // should be true
+}
+
+fun main() {
+    println(comparePeople())
 }
