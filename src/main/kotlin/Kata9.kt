6e70cc4ff3367e319267fe4506f96e88a1087df3
Enrique López Mañas <eenriquelopez@gmail.com>
2026-05-26 12:09:27 +0200

    Added one more example

--- src/main/kotlin/Kata9.kt
+++ src/main/kotlin/Kata9.kt
@@ -1,6 +1,9 @@
 fun Shop.getSetOfCustomers(): Set<Customer> =
     customers.toSet()
 
+fun Shop.getCustomersSortedByOrdersSize(): List<Customer> =
+    customers.sortedByDescending { it.orders.size }
+
 data class Shop(val name: String, val customers: List<Customer>)
 
 data class Customer(val name: String, val city: City, val orders: List<Order>) {
@@ -21,8 +24,13 @@ fun main() {
     val city = City(name = "Munich")
     val product = Product(name = "Laptop", price = 120.34)
     val order1 = Order(products = listOf(product), isDelivered = true)
-    val customer = Customer(name = "Allen", city = city, orders = listOf(order1))
-    val shop = Shop(name = "amazon", customers = listOf(customer, customer))
+    val customer = Customer(name = "Allen", city = city, orders = listOf(order1,order1,order1))
+    val secondCustomer = Customer("Max", city, orders = listOf(order1, order1))
+    val shop = Shop(name = "amazon", customers = listOf(customer, secondCustomer))
     val setOfCustomers = shop.getSetOfCustomers()
-    println(setOfCustomers)
+
+
+    println(shop.getCustomersSortedByOrdersSize())
+
+
 }
