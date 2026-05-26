336fb804ec13ed0ebe4958ccee78480cc0353c5c
Enrique López Mañas <eenriquelopez@gmail.com>
2026-05-26 09:40:05 +0200

    Added pair

--- src/main/kotlin/Kata6.kt
+++ src/main/kotlin/Kata6.kt
@@ -3,3 +3,8 @@ fun Int.r(): RationalNumber = TODO()
 fun Pair<Int, Int>.r(): RationalNumber = TODO()
 
 data class RationalNumber(val numerator: Int, val denominator: Int)
+
+data class Pair<out A, out B>(
+    val first: A,
+    val second: B
+)
